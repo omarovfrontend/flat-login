2e88dedae88d3684bb9e7dddb53d3f2b68d00f6b
Shamil Omarov <s_omarov05@mail.ru>
2021-01-15 01:02:53 +0300

    Сделал видимость пароля через JS

--- main.js
+++ main.js
@@ -1,16 +1,35 @@
 const eyesBtn = document.querySelector('.js-eyes-btn');
 const icon = eyesBtn.querySelector('i');
 const fieldPassword = document.querySelector('.subscribe-form-password');
+const fieldEmail = document.querySelector('.subscribe-form-email');
+const jsForm = document.querySelector('.js-form');
+const sendBtn = document.querySelector('.subscribe-form-btn');
 
-// вещаем обработчик события
+// вешаем обработчик события
 eyesBtn.addEventListener('click', function() {
-    if (icon.classList.contains('fa-eye')) {
+    if (icon.classList.contains('fa-lock')) {
         fieldPassword.setAttribute('type', 'text');
-        icon.classList.remove('fa-eye');
-        icon.classList.add('fa-eye-slash');
+        icon.classList.remove('fa-lock');
+        icon.classList.add('fa-unlock-alt');
     } else {
         fieldPassword.setAttribute('type', 'password');
-        icon.classList.remove('fa-eye-slash');
-        icon.classList.add('fa-eye');
+        icon.classList.remove('fa-unlock-alt');
+        icon.classList.add('fa-lock');
     }
 });
+
+jsForm.addEventListener('submit', formSubmit);
+
+sendBtn.addEventListener('click', formSubmit);
+
+function formSubmit(event) {
+    event.preventDefault();
+
+    if (fieldEmail.value && fieldPassword.value) {
+        alert('Форма отправлена!');
+        fieldEmail.value = '';
+        fieldPassword.value = '';
+    } else {
+        alert('Заполните все поля!');
+    }
+}
